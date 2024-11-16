6450910a8575b2b831e4b9c339a2f364b804e4a7
Herbie Vine <me@herbievine.com>
2024-11-16 17:47:06 +0100

    feat: nodejs adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bleu.dev',
   output: 'server',
   integrations: [react(), tailwind()],
-  adapter: cloudflare()
+  adapter: node({
+    mode: 'standalone'
+  })
 });
