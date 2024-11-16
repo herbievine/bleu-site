ca1fa7bc8f9e414e6c233a5bce7cbf2f4ac93bad
Herbie Vine <me@herbievine.com>
2024-11-16 18:30:37 +0100

    fix: rounded none 2

--- src/components/kit/inputs/input-05.tsx
+++ src/components/kit/inputs/input-05.tsx
@@ -9,7 +9,7 @@ export default function Input05() {
         placeholder="hello@example.com"
         defaultValue="hello@example"
         type="text"
-        className="w-full px-3 py-1.5 text-red-500 border border-red-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 ring-red-300"
+        className="w-full px-3 py-1.5 text-red-500 rounded-none border border-red-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2 ring-red-300"
       />
       <span className="text-red-500 text-xs">Invalid email address</span>
     </div>
