dac317dfb7069e68a39cdb55da8a85c791558311
Herbie Vine <me@herbievine.com>
2024-11-20 15:31:59 +0100

    feat: gen sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://poivre.dev',
   trailingSlash: 'never',
   output: 'server',
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({
+      filter: (page) => !page.includes('kit')
+    })
+  ],
   adapter: node({
     mode: 'standalone'
   })
